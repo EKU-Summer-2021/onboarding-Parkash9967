547571d2e72048e7188eef000f19caec96f997c7
Om Parkash <omparkashladhani@gmail.com>
2021-06-30 01:27:48 +0500

    feat:added read_data function

--- src/read_data.py
+++ src/read_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def read_data():
     df = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_360.csv')
@@ -18,7 +17,6 @@ def read_data1():
 
 read_data1()
 
-
 def read_data2():
     df = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_6200.csv.csv')
@@ -26,3 +24,4 @@ def read_data2():
 
 
 read_data2()
+

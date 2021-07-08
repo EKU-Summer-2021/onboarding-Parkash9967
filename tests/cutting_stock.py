4e939cb5d5f2c6a560eb6241ec6d3113c53d8215
Om Parkash <omparkashladhani@gmail.com>
2021-07-08 17:07:07 +0500

    fix : the unitest was updated

--- tests/cutting_stock.py
+++ tests/cutting_stock.py
@@ -11,25 +11,13 @@ class MyTestCase(unittest.TestCase):
      class of  unit test of cutting_stock
     """
 
-    def setUp(self):
-        """
-            function for getting the data
-        """
-        self.csp = CuttingStock('https://raw.githubusercontent.com/'
-                                'EKU-Summer-2021/intelligent_system_data/'
-                                'main/Intelligent%20System%20Data/CSP/CSP_360.csv', 60)
-
     def test(self):
         """
             function for testing the actual and expected values
         """
-        # given
-        expected_stock_size = 60
-        expected_piece_count = 13
-        # when
-        actual_stock_size = self.csp.stock_size
-        actual_piece_count = self.csp.count
-
-        # then
-        self.assertEqual(expected_stock_size, actual_stock_size)
-        self.assertEqual(expected_piece_count, actual_piece_count)
+        csp = CuttingStock('https://raw.githubusercontent.com/'
+                                'EKU-Summer-2021/intelligent_system_data/'
+                                'main/Intelligent%20System%20Data/CSP/CSP_360.csv', 60)
+        actual = csp.cutting_stock()
+        expected = 9
+        self.assertEqual(actual, expected)
